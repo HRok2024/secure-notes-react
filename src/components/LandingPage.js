8b2b1e7a36af74a790ab0b9189baae06a1d57a7a
rok <rok@gmail.com>
2025-02-11 09:37:08 +0900

    랜딩페이지 기본

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -7,15 +7,17 @@ import State from "./LandingPageCom/State";
 import Testimonial from "./LandingPageCom/Testimonial/Testimonial";
 import { useMyContext } from "../store/ContextApi";
 
+//위에서 내려오는 페이지
 const fadeInFromTop = {
   hidden: { opacity: 0, y: -50 },
   visible: { opacity: 1, y: 0, transition: { duration: 0.8 } },
 };
+//아래에서 올라오는 페이지
 const fadeInFromBotom = {
   hidden: { opacity: 0, y: 20 },
   visible: { opacity: 1, y: 0, transition: { duration: 0.5 } },
 };
-
+//컨텍스트에서 토큰을 가져온다
 const LandingPage = () => {
   // Access the token  state by using the useMyContext hook from the ContextProvider
   const { token } = useMyContext();
@@ -29,7 +31,7 @@ const LandingPage = () => {
           animate="visible"
           variants={fadeInFromTop}
         >
-          Turn your thoughts into secure, organized notes And Faster.
+          누구든지 원하는 노트를 작성해 보세요!
         </motion.h1>
         <h3 className="text-logoText md:text-2xl text-xl font-semibold text-slate-800 text-center">
           The #1 secure note-taking app.
@@ -45,6 +47,7 @@ const LandingPage = () => {
           variants={fadeInFromBotom}
           className="flex items-center justify-center gap-3 py-10 "
         >
+          {/* 토큰이 있으면 노트작성버튼, 없으면 로그인과 가입하기가 보인다 */}
           {token ? (
             <>
               <Link to="/create-note">
@@ -62,12 +65,12 @@ const LandingPage = () => {
             <>
               <Link to="/login">
                 <Buttons className="sm:w-52 w-44 bg-customRed font-semibold hover:scale-105 transition-all duration-200 cursor-pointer text-white px-10 py-3 rounded-sm">
-                  SignIn
+                  로그인
                 </Buttons>
               </Link>
               <Link to="/signup">
                 <Buttons className="sm:w-52 w-44 bg-btnColor font-semibold hover:scale-105 transition-all duration-200 cursor-pointer text-white px-10 py-3 rounded-sm">
-                  SignUp
+                  가입하기
                 </Buttons>
               </Link>
             </>
