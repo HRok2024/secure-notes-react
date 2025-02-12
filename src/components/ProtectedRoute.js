366d8d68258a5340b9b4e97321bc3451677cad52
rok <rok@gmail.com>
2025-02-12 11:22:07 +0900

    라우트 주석 수정

--- src/components/ProtectedRoute.js
+++ src/components/ProtectedRoute.js
@@ -7,12 +7,12 @@ const ProtectedRoute = ({ children, adminPage }) => {
   //컨텍스트로 토큰과 어드민확인 가져옴
   const { token, isAdmin } = useMyContext();
 
-  //토큰이 없으면 로그인으로
+  //1. 토큰이 없으면 로그인으로
   if (!token) {
     return <Navigate to="/login" />;
   }
 
-  //토큰이 있고 관리자페이지이지만 관리자가 아니면 디나이
+  //2. 토큰이 있고 관리자페이지이지만 관리자가 아니면 디나이
   if (token && adminPage && !isAdmin) {
     return <Navigate to="/access-denied" />;
   }
